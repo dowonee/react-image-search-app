7cdcc49d54b3ab51ad4938901bb33284cca972f4
dowonee <sssjdw@naver.com>
2024-12-28 19:43:43 +0900

    Feat: add search bar component

---
+++ src/components/SearchBar/index.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export default function SearchBar({handleSearchImage, searchWord, setSearchWord}) {
+  const inputSearchWord = (ev) => {
+    setSearchWord(ev.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={(ev) => handleSearchImage(ev, searchWord)}>
+        <input
+          type="text"
+          placeholder="검색어를 입력하세요"
+          onChange={inputSearchWord}
+          value={searchWord}
+        />
+        <button>검색</button>
+      </form>
+    </div>
+  );
+}
